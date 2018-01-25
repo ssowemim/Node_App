8e424daefec33dbd42cdd4a72debc54c69ebd178
Femi Sowemimo <ssowemim@ualberta.ca>
2018-01-25 12:45:06 -0700

    finished picture upload now

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -1,18 +1,20 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
 const multerOptions = {
     storage: multer.memoryStorage(),
-    fileFilter: function(req, file, next) {
-        const isPhoto = file.mimeType._startsWith('image/');
+    fileFilter(req, file, next) {
+        const isPhoto = file.mimetype.startsWith('image/');
         if(isPhoto) {
-            // has a first value, means false - if first value is null and second true then means - yea we good. keep it moving!
             next(null, true);
         } else {
-            next({message: 'That filetype isn\'t allowed!'}, false);
+            next({ message: 'That filetype isn\'t allowed!' }, false);
         }
     }
-}
+};
 
 exports.homePage = (req, res) => {
     res.render('index');
@@ -23,6 +25,22 @@ exports.addStore = (req, res) => {
 };
 
 exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+    // check if there is no new file to resize
+    if (!req.file) {
+        next(); // skip to the next middleware
+        return;
+    }
+    const extension = req.file.mimetype.split('/')[1];
+    req.body.photo = `${uuid.v4()}.${extension}`;
+    // now we resize
+    const photo = await jimp.read(req.file.buffer);
+    await photo.resize(800, jimp.AUTO);
+    await photo.write(`./public/uploads/${req.body.photo}`);
+    // once we have written the photo to our filesystem, keep going!
+    next();
+};
 
 exports.createStore = async (req, res) => {
     const store = await (new Store(req.body)).save();
@@ -46,9 +64,8 @@ exports.editStore = async (req, res) => {
 };
 
 exports.updateStore = async (req, res) => {
-    // setting location data to be saved as a point
+    // set the location data to be a point
     req.body.location.type = 'Point';
-
     // find and update the store
     const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
         new: true, // return the new store instead of the old one
