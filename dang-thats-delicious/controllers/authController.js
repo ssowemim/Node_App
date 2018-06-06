9b81b16ded6048b82dd56099b20ef3f57769bb33
Femi Sowemimo <ssowemim@ualberta.ca>
2018-06-06 10:13:12 -0600

    Finished users Login/Logout screen

--- dang-thats-delicious/controllers/authController.js
+++ dang-thats-delicious/controllers/authController.js
@@ -6,3 +6,18 @@ exports.login = passport.authenticate('local', {
     successFlash: 'You are now logged in!',
     successRedirect: '/'
 });
+
+exports.logout = (req, res) => {
+    req.logOut();
+    req.flash('success', 'You are now logged out!');
+    res.redirect('/');
+}
+
+exports.isLoggedIn = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        next();
+        return;
+    }
+    req.flash('error', 'Oops you must be logged in!');
+    res.redirect('/login');
+}
